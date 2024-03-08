d1152e7908bde2425e8d256b75177716c4267926
mudjakils <rheakeah07@gmail.com>
2024-03-08 08:53:44 +0800

    added card tab 1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,24 +1,22 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="src/img/reggie.png" />
+      <IonCardHeader>
+        <IonCardTitle>Reggie</IonCardTitle>
+        <IonCardSubtitle>wala gwapo lang</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>Minsan may ni like akong status nang babae kase natawa ako.
+After 10 seconds nagpadala sya nang Relationship Request.
+Hay Buhay.</IonCardContent>
+    </IonCard>
   );
 };
 
