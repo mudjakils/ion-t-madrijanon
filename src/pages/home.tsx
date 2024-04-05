d6de0f0595ceeb3933dba582d6fcecb35263bc46
mudjakils <20191554@nbsc.edu.ph>
2024-04-05 16:05:27 +0800

    added components in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,42 @@
 import {
-    //Initial Components
-    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonImg
 } from '@ionic/react';
 
-// CSS
-//import './profile.css';
-  
-  const Home: React.FC = () => {
-    return (
+const Home: React.FC = () => {
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Home</IonTitle>
-            </IonToolbar>
+          <IonHeader>
+              <IonToolbar>
+                  <IonTitle>Home</IonTitle>
+              </IonToolbar>
           </IonHeader>
-        </IonContent>
+          <IonContent fullscreen className="home-content">
+              <IonHeader collapse="condense">
+                  <IonToolbar>
+                      <IonTitle size="large">Home</IonTitle>
+                  </IonToolbar>
+              </IonHeader>
+
+              {/* Content */}
+              <IonCard>
+                  <IonCardHeader>
+                      <IonCardSubtitle className="bold-text">Welcome to my First App!</IonCardSubtitle>
+                      <br/>
+                      <IonImg src="../src/img/reggie2.png" alt="Image" />
+                      <br/>
+                      <IonCardTitle>This is the home page</IonCardTitle>
+                  </IonCardHeader>
+
+                  <IonCardContent>
+                      <p>This is where you can display your home page content.</p>
+                      <IonButton expand="block" routerLink="/profile">Go to Profile</IonButton>
+                  </IonCardContent>
+              </IonCard>
+
+          </IonContent>
       </IonPage>
-    );
-  };
-  
-  //
-  export default Home;
-  
+  );
+};
+
+export default Home;
