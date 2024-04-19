8f0f9844f698504e6080f1bb2b32214819582856
mudjakils <lomponkeneth02@gmail.com>
2024-04-19 10:41:03 +0800

    add todolist tsx, add todolist.css, info,change calulator, add image and make chnages in apptsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,87 @@
+import React from 'react';
 import {
   IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
   IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonImg
 } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const history = useHistory();
+
+  const handleCardClick = (route: string) => {
+    // Navigate to the desired route when the card is clicked
+    history.push(route);
+  };
+
   return (
-      <IonPage>
-          <IonHeader>
-              <IonToolbar>
-                  <IonTitle>Home</IonTitle>
-              </IonToolbar>
-          </IonHeader>
-          <IonContent fullscreen className="home-content">
-              <IonHeader collapse="condense">
-                  <IonToolbar>
-                      <IonTitle size="large">Home</IonTitle>
-                  </IonToolbar>
-              </IonHeader>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="home-content">
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Home</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-              {/* Content */}
-              <IonCard>
-                  <IonCardHeader>
-                      <IonCardSubtitle className="bold-text">Welcome to my First App!</IonCardSubtitle>
-                      <br/>
-                      <IonImg src="../src/img/reggie2.png" alt="Image" />
-                      <br/>
-                      <IonCardTitle>This is the home page</IonCardTitle>
-                  </IonCardHeader>
+        {/* First Card */}
+        <IonButton onClick={() => handleCardClick('/calculator')} fill="clear">
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle className="bold-text"></IonCardSubtitle>
+              <IonCardTitle>Calculator</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              {/* No content here */}
+            </IonCardContent>
+          </IonCard>
+        </IonButton>
 
-                  <IonCardContent>
-                      <p>This is where you can display your home page content.</p>
-                      <IonButton expand="block" routerLink="/profile">Go to Profile</IonButton>
-                  </IonCardContent>
-              </IonCard>
+        {/* Second Card */}
+        <IonButton onClick={() => handleCardClick('/click-counter')} fill="clear">
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle className="bold-text"></IonCardSubtitle>
+              <IonCardTitle>Click Counter</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              {/* No content here */}
+            </IonCardContent>
+          </IonCard>
+        </IonButton>
 
-          </IonContent>
-      </IonPage>
+        {/* Third Card */}
+        <IonButton onClick={() => handleCardClick('/todo-list')} fill="clear">
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle className="bold-text"></IonCardSubtitle>
+              <IonCardTitle>To-Do List</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              {/* No content here */}
+            </IonCardContent>
+          </IonCard>
+        </IonButton>
+
+        {/* Fourth Card */}
+        <IonButton onClick={() => handleCardClick('#')} fill="clear">
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle className="bold-text"></IonCardSubtitle>
+              <IonCardTitle>Blank</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              {/* No content here */}
+            </IonCardContent>
+          </IonCard>
+        </IonButton>
+        
+
+      </IonContent>
+    </IonPage>
   );
 };
 
